df371ca9013e389411652a9928c7d367ae2203a0
Roberto Tapia <roberto.tapia@ug.uchile.cl>
2021-02-22 19:48:42 -0300

    Create BlogDetailView

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_posts_list'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
