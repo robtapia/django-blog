e4b79f7ed68fcd310b395de29218bcc740753b1a
Roberto Tapia <roberto.tapia@ug.uchile.cl>
2021-02-23 20:31:36 -0300

    Create BlogUpdateView and setup

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -16,3 +16,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
