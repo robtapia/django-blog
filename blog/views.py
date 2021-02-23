44afb8d43ff9afbd9b1038054acacb68c7b88887
Roberto Tapia <roberto.tapia@ug.uchile.cl>
2021-02-23 20:41:54 -0300

    Create BlogDeleteView and setup

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -21,3 +23,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
