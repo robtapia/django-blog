5b06b32931a85896becb18f31e8ac492508fc08e
Roberto Tapia <roberto.tapia@ug.uchile.cl>
2021-02-24 19:34:57 -0300

    Heroku config setup

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iwwrryh-(7eb!-1@j68aythb4tuzhx9)12(^0ke_=s(0e$r#1$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
